e06b09297ea96c57cc6865423f306de7da866095
sask <angbmz364@gmail.com>
2026-05-07 16:38:22 -0500

    feat: simple navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Highlights from './components/Highlights.jsx'
+
+export default function App() {
+  return (
+    <main className="">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
